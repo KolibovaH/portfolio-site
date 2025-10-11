cae66a5a2a234f267c93a19e6b2e9cfb4ba05a83
Hana Kolibova <h.kolibova@gmail.com>
2025-10-11 11:07:23 +0200

    Initial Docusaurus portfolio

---
+++ sidebars.js
@@ -0,0 +1,9 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Portfolio',
+      items: ['intro', 'api-docs', 'conceptual', 'user-guide', 'getting-started', 'how-to', 'tutorial', 'troubleshooting'], 
+    },
+  ],
+};
